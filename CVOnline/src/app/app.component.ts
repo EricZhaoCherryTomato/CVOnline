2da4c4ebd1b231e0e3c65f7078c673a4b3bee95f
Eric.Zhao <eric.ye.zhao@gmail.com>
2017-06-30 15:52:52 +1000

    Tile change

--- CVOnline/src/app/app.component.ts
+++ CVOnline/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Eric Ye Zhao Online CV';
+  title = 'Eric Zhao e-Resume';
 
    private itemsList:Object[] = [
         {
