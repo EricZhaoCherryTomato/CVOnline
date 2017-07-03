0ef2e8153422e45a196465e869c421296e3d3b74
Eric.Zhao <Eric.Zhao@dominos.com.au>
2017-07-03 13:58:59 +1000

    more context

--- CVOnline/src/app/app.component.ts
+++ CVOnline/src/app/app.component.ts
@@ -11,13 +11,23 @@ export class AppComponent {
    private itemsList:Object[] = [
         {
           title: 'Jan 2015 - Current   Domino’s Pizza Enterprise',
-          projects: [ { name : 'Star Rating', tech: 'KnockoutJS'},
-                      { name : 'Online Order Responsive Redesign', tech: 'SASS HTML KnockoutJS'}]
+          projects: [ { name : 'Dominos Team App', tech: 'Angular, TypeScript, Gulp, AWS, C# Web Api, Autofac DI'},
+                      { name : 'Star Rating', tech: 'KnockoutJS, AJAX, CSS'},
+                      { name : 'Online Order Responsive Redesign', tech: 'SASS, HTML5, KnockoutJS'},
+                      { name : 'Timed Guaranteed Product Delivery', tech: 'JQuery, SASS, HTML5'},
+                      { name : 'New Payment Gateway', tech: 'C# Web Api'},
+                      { name : 'SMS ordering', tech: 'C# Web Api'},]
         },
         {
-          title: 'Mar 2014 - Jan 2015   Axial Group',
-          projects: [ { name : 'Internal and Public site', tech: 'JQuery HTML'},
-                      { name : 'Student Enrollment System', tech: 'JQuery CSS HTML'}]
+          title: 'Mar 2014 - Jan 2015   Axial Group Ltd',
+          projects: [ { name : 'Internal and Public website', tech: 'JQuery HTML5'},
+                      { name : 'Student Enrollment System', tech: 'JQuery CSS HTML5'},
+                      { name : 'Purchase Order System', tech: 'JQuery CSS HTML5'}]
+        },
+        {
+          title: 'Dec 2012 - Mar 2014   Zidea Robot Co. Ltd',
+          projects: [ { name : 'Smart Warehouse', tech: 'VS2010, C#, SQL'}
+                    ]
         }
     ]
     
