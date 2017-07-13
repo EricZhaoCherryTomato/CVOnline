fa15066a737363df76eeb1ae9b652f0d4d2243ca
Ye Zhao <zhaoye@Zhaos-MacBook-Pro.local>
2017-07-13 10:46:31 +1000

    note update

--- CVOnline/src/app/app.component.ts
+++ CVOnline/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Eric Zhao e-Resume';
-  //note = 'Accenture and ATO is Awsome';
+  note = 'Accenture and ATO is Awsome';
 
    private itemsList:Object[] = [
         {
